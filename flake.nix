033e787e77b67b071226cf1699ea5af67b2ace9b
Curt Bushko <cbushko@gmail.com>
2026-01-16 23:28:02 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Golang flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 25; # Change this to update the whole stack
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ self.overlays.default ];
+        };
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        go = final."go_1_${toString goVersion}";
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            docker
+            # go (version is specified by overlay)
+            go
+            gotools
+            golangci-lint
+          ];
+        };
+      });
+    };
+}
